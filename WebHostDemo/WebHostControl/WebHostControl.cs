ec73142fc69a0f9f28802e4fa6dc9ea7287a6ac5
Wladimir <wladimir.melnik@gmail.com>
2017-09-11 18:56:17 +0200

    add editor. improve loading of css and js files

--- WebHostDemo/WebHostControl/WebHostControl.cs
+++ WebHostDemo/WebHostControl/WebHostControl.cs
@@ -12,14 +12,24 @@ namespace WebHostDemo
     {
         static string _script = "";
         static string _basePage = "";
-        static string _3dScript = "&3dScript";
+        static string _d3Script = "_d3Script";
+        static string _stylePath = "_styleFile";
 
         static WebHostControl()
         {
-            _script = File.ReadAllText("WebHostControl/d3.js");
-            _basePage = File.ReadAllText("WebHostControl/BaseHTML.html");
+            string filePath = @"file:///" + AppDomain.CurrentDomain.BaseDirectory.Replace('\\', '/');
 
-            _basePage = _basePage.Replace(_3dScript, _script);
+            //_basePage = File.ReadAllText("WebHostControl/BaseHTML.html");
+            //_basePage = File.ReadAllText("WebHostControl/Gears.html");
+            //_script = File.ReadAllText("WebHostControl/d3.v3.min.js");
+
+            //_basePage = File.ReadAllText("WebHostControl/GanttChart.html");
+            //_basePage = _basePage.Replace(_stylePath, filePath + @"WebHostControl/style.css");
+            //_basePage = _basePage.Replace(_d3Script, filePath + @"WebHostControl/d3.v3.min.js");
+
+            _basePage = File.ReadAllText("WebHostControl/MarkdownEditor.html");
+            _basePage = _basePage.Replace(_stylePath, filePath + @"WebHostControl/simplemde.min.css");
+            _basePage = _basePage.Replace(_d3Script, filePath + @"WebHostControl/simplemde.min.js");
         }
 
         public WebHostControl()
